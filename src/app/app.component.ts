52168f2b29aa47f3d897c376362ba19189eada88
Alan Ruiz <alan.c.ruiz.70@gmail.com>
2025-07-15 10:11:19 -0600

    actualizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./footer/footer.component";
+import { ListComponent } from "./list/list.component";
+import { SearchBarComponent } from "./search-bar/search-bar.component";
+import { BannerComponent } from "./banner/banner.component";
+import { HeaderComponent } from "./header/header.component";
+import { LibraryService } from './library.service';
+import { Book } from './book';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, ListComponent, SearchBarComponent, BannerComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'libreria';
+
+  myBookList:Book[] = [];
+
+ //se inyecta el servicio al componente
+ constructor(private libraryService:LibraryService) {
+ }
+ ngOnInit() {
+  this.loadInitialBooks(); // Carga los libros al inicio
+ }
+ // Muestra todos los libros inicialmente
+ loadInitialBooks() {
+  this.myBookList = this.libraryService.getBooks();
+ }
+
+ searchEvent(eventList:Book[]){
+  this.myBookList = eventList;
+  console.log(this.myBookList);
+ }
 }
+
---
+++ src/app/list/list.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Book } from '../book';
+
+@Component({
+  selector: 'app-list',
+  imports: [],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+  @Input() bookList:Book[]=[];
+}
